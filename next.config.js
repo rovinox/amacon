894eeb45b4580b1c93655204964222125874f036
neaj mahmud <neajmahmud230@gmail.com>
2019-10-14 16:55:22 -0400

    added mongodb

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV: "<insert-mongo-srv>",
+    MONGO_SRV: "mongodb+srv://rovinox:JzxyGxvFqISPtcf9@rovinox-yqbzz.mongodb.net/test?retryWrites=true&w=majority",
     JWT_SECRET: "<insert-jwt-secret>",
     CLOUDINARY_URL: "<insert-cloudinary-url>",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
