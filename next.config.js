d5b44051cb2d08f9990d75d42ea225b3901f578c
neaj mahmud <neajmahmud230@gmail.com>
2019-10-14 22:23:14 -0400

    added file to ignore

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// must restart server whenever you make changes in next.config
-module.exports = {
-  env: {
-    MONGO_SRV: "mongodb+srv://rovinox:JzxyGxvFqISPtcf9@rovinox-yqbzz.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
-    CLOUDINARY_URL: "<insert-cloudinary-url>",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
-  }
-};
